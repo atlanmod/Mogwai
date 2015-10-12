fd707b1dc5931cbe742e5805c3c8292104b46754
gwendal <gwendal.daniel@gmail.com>
2015-10-12 16:44:54 +0200

    update pom & rename OCL folder

--- core/src/main/java/fr/inria/atlanmod/mogwai/resources/MogwaiResourceImpl.java
+++ core/src/main/java/fr/inria/atlanmod/mogwai/resources/MogwaiResourceImpl.java
@@ -26,25 +26,21 @@ public class MogwaiResourceImpl extends PersistentResourceImpl implements Mogwai
         assert this.persistenceBackend instanceof BlueprintsPersistenceBackend;
     }
 
-    @Override
     public MogwaiQueryResult query(URI oclFileURI, Object obj) {
         Constraint exp = MogwaiUtil.parseOCL(oclFileURI,this);
         return this.query(exp,obj);
     }
     
-    @Override
     public MogwaiQueryResult query(Constraint exp, Object obj) {
         Mogwai mogwai = new Mogwai();
         return mogwai.performQuery(exp, obj, this, (BlueprintsPersistenceBackend)persistenceBackend);
     }
     
-    @Override
     public MogwaiQueryResult query(URI oclFileURI) {
         Constraint exp = MogwaiUtil.parseOCL(oclFileURI, this);
         return this.query(exp,null);
     }
     
-    @Override
     public MogwaiQueryResult query(Constraint exp) {
         return this.query(exp,null);
     }
