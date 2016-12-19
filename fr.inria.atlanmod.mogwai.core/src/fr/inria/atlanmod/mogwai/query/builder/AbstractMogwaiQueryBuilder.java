059374108197c52c83bd9a6720efc132ee0e3bcf
gwendal <gwendal.daniel@gmail.com>
2016-12-19 19:02:47 +0100

    add validation check on query build()

--- fr.inria.atlanmod.mogwai.core/src/fr/inria/atlanmod/mogwai/query/builder/AbstractMogwaiQueryBuilder.java
+++ fr.inria.atlanmod.mogwai.core/src/fr/inria/atlanmod/mogwai/query/builder/AbstractMogwaiQueryBuilder.java
@@ -5,48 +5,51 @@ import java.io.File;
 import org.eclipse.emf.common.util.URI;
 
 import fr.inria.atlanmod.mogwai.query.MogwaiQuery;
+import fr.inria.atlanmod.mogwai.query.MogwaiQueryException;
 
 public abstract class AbstractMogwaiQueryBuilder<B extends AbstractMogwaiQueryBuilder<B>> {
-	
+
 	protected Object input;
-	
+
 	protected AbstractMogwaiQueryBuilder() {
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected B me() {
 		return (B) this;
 	}
-	
+
 	/**
 	 * @return {@code true} if no conflict is detected
 	 */
 	protected boolean validate() {
 		return true;
 	}
-	
+
 	public B fromFile(File file) {
 		this.input = file;
 		return me();
 	}
-	
+
 	public B fromString(String string) {
 		this.input = string;
 		return me();
 	}
-	
+
 	public B fromURI(URI uri) {
 		this.input = uri;
 		return me();
 	}
-	
+
 	public MogwaiQuery build() {
-		validate();
-		
-		return buildQuery();
+		if (validate()) {
+			return buildQuery();
+		} else {
+			throw new MogwaiQueryException("An error occured when creating the query with the provided parameters");
+		}
 	}
-	
+
 	protected abstract MogwaiQuery buildQuery();
-	
+
 }
