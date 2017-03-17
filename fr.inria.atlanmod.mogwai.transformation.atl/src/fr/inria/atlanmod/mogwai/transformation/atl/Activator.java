952a490a5d7ddf5c2192b0c38a765b04af10e275
gwendal <gwendal.daniel@gmail.com>
2017-03-17 16:28:25 +0100

    remove NeoEMF dependency

--- fr.inria.atlanmod.mogwai.transformation.atl/src/fr/inria/atlanmod/mogwai/transformation/atl/Activator.java
+++ fr.inria.atlanmod.mogwai.transformation.atl/src/fr/inria/atlanmod/mogwai/transformation/atl/Activator.java
@@ -3,8 +3,6 @@ package fr.inria.atlanmod.mogwai.transformation.atl;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-import fr.inria.atlanmod.neoemf.logging.NeoLogger;
-
 
 public class Activator extends Plugin {
 	
@@ -13,20 +11,17 @@ public class Activator extends Plugin {
 	public static final String PLUGIN_ID = "fr.inria.atlanmod.mogwai.transformation.atl";
 	
 	public Activator() {
-		NeoLogger.debug("Mogwai ATL Transformation plugin activated");
 	}
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		NeoLogger.debug("Mogwai ATL Transformation plugin started");
 		plugin = this;
 	}
 	
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-		NeoLogger.debug("Mogwai ATL Transformation plugin stopped");
 		super.stop(context);
 	}
 	
